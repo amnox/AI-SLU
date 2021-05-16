8bf35384b349acc0530a2239cb8398710e487d85
Vallab Karanam <k.vallab6@gmail.com>
2021-05-15 20:43:03 -0500

    filter by dept and filter

--- src/components/facultyContent.js
+++ src/components/facultyContent.js
@@ -1,61 +1,5 @@
-/*
 import React, { Component } from "react";
-import {BACKEND_URL} from '../constants'
-
-class FacultyContent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: []
-        };
-    }
-    componentDidMount() {
-        fetch(BACKEND_URL+"faculty")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                this.setState({
-                    isLoaded: true,
-                    items: result.items
-                });
-            },
-            (error) => {
-                console.log("Inside error");
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        )
-    }
-
-    render() {
-        console.log(this.state);
-        const {error, isLoaded, items} = this.state;
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Loading...</div>;
-        } else {
-            return (
-                <ul>
-                    {items.map(item => (
-                        <li key={item.id}>
-                            {item.firstname} {item.lastname}
-                        </li>
-                    ))}
-                </ul>
-            );
-        }
-    }
-}
-
-export default FacultyContent;
-*/
-
-import React, { Component } from "react";
+import { Button } from "@material-ui/core";
 
 class Entry extends Component {
   constructor(props) {
@@ -64,10 +8,15 @@ class Entry extends Component {
   render() {
     const item = this.props.item;
     return (
-      <p className="faculty-item">
-        {item.firstname} {item.lastname}, {item.department}
-        {item.email} {item.number}
-      </p>
+      <div>
+        <div style={{ fontSize: 30, fontWeight: "bold" }}>
+          {item.firstname} {item.lastname}
+        </div>
+        {item.department}
+        {"       "}
+        {item.email}
+        {"       "} {item.number}
+      </div>
     );
   }
 }
@@ -75,17 +24,78 @@ class Entry extends Component {
 class FacultyContent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      department: "all",
+    };
   }
 
   render() {
-    const items = this.props.items;
+    var items_master = this.props.items;
+    if (this.state.department == "all") {
+      items = items_master;
+    } else if (this.state.department == "Computer Science") {
+      var items = items_master.filter(
+        (item) => item.department == "Computer Science"
+      );
+    } else if (this.state.department == "Engineering") {
+      var items = items_master.filter(
+        (item) => item.department == "Engineering"
+      );
+    } else if (this.state.department == "Business") {
+      var items = items_master.filter((item) => item.department == "Business");
+    } else if (this.state.department == "Health") {
+      var items = items_master.filter((item) => item.department == "Health");
+    }
+
     return (
-      <div className="faculty-container">
-        <h1>Faculty</h1>
-        {items.map((item) => (
-          <Entry key={item.lastname} item={item} />
-        ))}
-      </div>
+      <React.Fragment>
+        <div className="faculty-container">
+          <h1>Faculty</h1>
+          {items.map((item) => (
+            <Entry key={item._id} item={item} />
+          ))}
+        </div>
+        <Button
+          onClick={() => {
+            this.setState({ department: "all" });
+          }}
+          variant="contained"
+        >
+          All Departments
+        </Button>
+        <Button
+          onClick={() => {
+            this.setState({ department: "Computer Science" });
+          }}
+          variant="contained"
+        >
+          Computer Science
+        </Button>
+        <Button
+          onClick={() => {
+            this.setState({ department: "Engineering" });
+          }}
+          variant="contained"
+        >
+          Engineering
+        </Button>
+        <Button
+          onClick={() => {
+            this.setState({ department: "Business" });
+          }}
+          variant="contained"
+        >
+          Business
+        </Button>
+        <Button
+          onClick={() => {
+            this.setState({ department: "Health" });
+          }}
+          variant="contained"
+        >
+          Health
+        </Button>
+      </React.Fragment>
     );
   }
 }
